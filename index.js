ff7f7da287c3eaa4e762fa344d2814528bd5ee61
princessacintaqia <cintaqia@gmail.com>
2021-09-11 21:38:49 +0700

    Changed main city to Las Vegas

--- index.js
+++ index.js
@@ -78,4 +78,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-searchCity("Jakarta");
+searchCity("Las Vegas");
